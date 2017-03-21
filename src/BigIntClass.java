d49a9e47f2567105b155a28a3723e0a979a03d82
mohammadHossein <mohammadHossein@hossein-pc>
2017-03-21 15:51:13 +0330

    سلام

---
+++ src/BigIntClass.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mohammadHossein
+ */
+public class BigIntClass {
+
+    //############################ Attributes
+    private String BigArry;
+    char sign;
+
+    //############################ Methods
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Constructors
+    public BigIntClass() {
+        //تکمیل شود
+    }
+
+    public BigIntClass(String str) {
+        //تکمیل شود
+    }
+
+    public BigIntClass(long b) {
+        //تکمیل شود
+    }
+
+//construct clone of BigIntClass
+    public BigIntClass(BigIntClass x) {
+        //تکمیل شود
+    }
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Setters
+
+    public void setBigInt(String str) {
+        //تکمیل شود
+    }
+
+    public void setBigInt(BigIntClass b) {
+        //تکمیل شود
+    }
+
+    public void setBigInt(long x) {
+        //تکمیل شود
+    }
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Getters
+    public String getBigInt() {
+        //تکمیل شود
+        return "";
+    }
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ other methods
+ public void setSumBigInt(BigIntClass x , BigIntClass y){
+     //تکمیل شود
+ }
+public void setMinusBigInt(BigIntClass x , BigIntClass y){
+    //تکمیل شود
+}
+public void setMultiBigInt(BigIntClass x , BigIntClass y){
+    //تکمیل شود
+}
+public void setDivideBigInt(BigIntClass x , BigIntClass y){
+    //تکمیل شود(اختیاری) 
+}
+
+}
